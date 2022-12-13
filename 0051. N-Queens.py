c22b64bc04c4b065b9d02706055c1067c257d07c
Krishna Sai Tarun Pasupuleti <kpasupul@usc.edu>
2022-12-13 01:21:08 -0800

    Create 0051. N-Queens.py
    The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
    
    Given an integer n, return all distinct solutions to the n-queens puzzle. You may return the answer in any order.
    
    Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space, respectively.
    
     
    
    Example 1:
    
    
    Input: n = 4
    Output: [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
    Explanation: There exist two distinct solutions to the 4-queens puzzle as shown above
    Example 2:
    
    Input: n = 1
    Output: [["Q"]]
     
    
    Constraints:
    
    1 <= n <= 9

---
+++ 0051. N-Queens.py
@@ -0,0 +1,97 @@
+class Solution:
+
+    ans = []
+
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        chess_board = [[0 for i in range(n)] for j in range(n)] 
+        possible_solutions = []
+        taken_rows = set()
+
+        def in_attack_zone(row, col) -> bool:
+            nonlocal chess_board
+            return True if chess_board[row][col] > 0 else False 
+        
+        def get_position_string(i) -> str:
+            return "." * i + "Q" + "." * (n-i-1)
+        
+        # def place_queen(row, col):
+        #     nonlocal chess_board
+
+        #     chess_board[row][col] += 1
+        #     for i in range(n):
+        #         chess_board[i][col]+=1
+        #         chess_board[row][i]+=1
+            
+        #     i = row
+        #     j = col 
+        #     while i < n and j <n:
+        #         chess_board[i][j]+= 1
+        #         i+=1 
+        #         j+=1
+                        
+        #     i = row
+        #     j = col 
+        #     while i >=0 and j>= 0:
+        #         chess_board[i][j]+= 1
+        #         i-=1
+        #         j-=1
+
+
+        # def remove_queen(row, col):
+        #     nonlocal chess_board
+        #     chess_board[row][col] -= 1
+        #     for i in range(n):
+        #         chess_board[i][col] -=1
+        #         chess_board[row][i] -=1
+            
+        #     i = row
+        #     j = col 
+        #     while i < n and j <n:
+        #         chess_board[i][j] -= 1
+        #         i+=1 
+        #         j+=1
+            
+        #     i = row
+        #     j = col 
+        #     while i >=0 and j>= 0:
+        #         chess_board[i][j] -= 1
+        #         i-=1
+        #         j-=1
+    
+
+        def backtrack(row, current_solution, cols, diagonals, anti_diagonals):
+
+            if row != len(current_solution):
+                #couldn't place in previous row 
+                return 
+
+            if row == n:
+                possible_solutions.append([row for row in current_solution])
+                return 
+
+            # for i in range(n):
+            #     if not in_attack_zone(row, i):
+            #         place_queen(row, i)
+            #         current_solution.append(get_position_string(i))                        
+            #         backtrack(row+1, current_solution)
+            #         # if program comes here, we need to remove 
+            #         current_solution.pop()
+            #         remove_queen(row, i)
+
+            for i in range(n):
+                cur_diag = row - i
+                cur_anti_diag = row + i
+                if i in cols or cur_diag in diagonals or cur_anti_diag in anti_diagonals:
+                    continue
+                cols.add(i)
+                diagonals.add(cur_diag)
+                anti_diagonals.add(cur_anti_diag)
+                current_solution.append(get_position_string(i)) 
+                backtrack(row+1, current_solution, cols, diagonals, anti_diagonals)
+                cols.remove(i)
+                anti_diagonals.remove(cur_anti_diag)
+                diagonals.remove(cur_diag)
+                current_solution.pop()
+                
+        backtrack(0, [], set(), set(), set()) 
+        return possible_solutions
